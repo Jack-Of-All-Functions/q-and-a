eacc749fe7752f8068a368403be566a5565181ef
evanschwarz90 <53870272+evanschwarz90@users.noreply.github.com>
2020-08-27 13:14:11 -0600

    Update schema.js
    comments to consider

--- db/schema.js
+++ db/schema.js
@@ -3,6 +3,7 @@ const pgSchema = {
   //Question Table
   questions: `CREATE TABLE IF NOT EXISTS questions (
     question_id SERIAL Primary Key,
+    // Using "question_" for these fields is redundant. Since they are columns in the "questions" table it is assumed that they belong to a question unless otherwise specified (like product_id)
     product_id INT UNIQUE not NULL,
     question_body VARCHAR(255) not NULL,
     question_date TIMESTAMP not NULL,
@@ -10,23 +11,28 @@ const pgSchema = {
     question_helpfulness INT not NULL,
     reported BOOLEAN,
     answers INTEGER []
+    // I think what you're trying to do here is make a two way connection between questions and their answers. Having a foreign key in the answers table should be sufficient to create the relationship
   );`,
 
   //Answers Table
   answers: `CREATE TABLE IF NOT EXISTS answers (
     answer_id SERIAL Primary Key,
+    // same note as above about "answer_"
     question_id INT UNIQUE not NULL,
     answer_body VARCHAR(255) not NULL,
     answer_date TIMESTAMP not NULL,
     answerer_name VARCHAR(50) not NULL,
     answer_helpfulness INT not NULL,
     answer_photos TEXT [],
+    // While this might work I typically do this with a separate table. Up to you if you want to try this approach or make a table to hold photos with a reference to an answer. I've never done it this way so I won't steer you away if you want to try this approach
     FOREIGN KEY (question_id)
     REFERENCES questions (question_id)
+    // Perfect. This eliminates the need for an "answers" column in your questions table
   );`
 }
 
 const mgSchema = {
+  // I would name this QuestionsSchema and remove "question_" like in postgres
   question_id: mongoose.ObjectId,
   product_id: Number,
   question_body: String,
@@ -35,13 +41,16 @@ const mgSchema = {
   question_helpfulness: Number,
   reported: Boolean,
   answers: {
+  // I'm thinking this would need to be an array of objects -- [{ answer_id: Number, etc... }]
     answer_id: Number,
+    // I could see this helping with clarity but I would consider taking "answer_" off here as well since it should be clear since they are in the "answers" property. Either way is fine here I think
     question_id: mongoose.ObjectId,
     answer_body: String,
     answer_date: Date,
     answerer_name: String,
     answer_helpfulness: Number,
     answer_photos: String
+    // array of strings here? [String] maybe (not 100% sure on that syntax)
   }
 }
 
